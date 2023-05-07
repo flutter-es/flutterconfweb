44d70556b8aa88ffe1bc0e0e4e8e17bebcd3dbef
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2023-05-06 22:39:53 -0500

    config localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_conf_colombia/ui/pages/home_page.dart';
+import 'package:flutter_conf_colombia/ui/internations/translation.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const FlutterCon());
 }
 
-class FlutterCon extends StatelessWidget {
+class FlutterCon extends StatefulWidget {
   const FlutterCon({super.key});
+
+  static void setLocale(BuildContext context, Locale newLocale) {
+    final state = context.findAncestorStateOfType<_FlutterConState>()!;
+    state.changeLanguage(newLocale);
+  }
+
+  @override
+  State<FlutterCon> createState() => _FlutterConState();
+}
+
+class _FlutterConState extends State<FlutterCon> {
+  Locale _locale = Translation.es;
+
+  void changeLanguage(Locale newLocal) {
+    setState(() {
+      _locale = newLocal;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +36,17 @@ class FlutterCon extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      locale: _locale,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Translation.en,
+        Translation.es,
+      ],
+      home: const HomePage(),
     );
   }
 }
