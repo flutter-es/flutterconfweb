bc4cb358c160b22de7d6e3213b61b9011be97b7b
Roman Jaquez <romanjaquez@Romans-MacBook-Pro.local>
2023-05-14 21:28:33 -0400

    fetch banners upon auto-scrolling

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -55,7 +55,7 @@ class _HomePageState extends ConsumerState<HomePage>
   }
 
   void setupSections() {
-    sections = ref.watch(homeSectionsProvider);
+    sections = ref.refresh(homeSectionsProvider);
 
     tabController = CustomTabController(
       length: sections.length,
@@ -132,6 +132,9 @@ class _HomePageState extends ConsumerState<HomePage>
 
   void moveSectionByIndex(int index, {bool isMobile = false}) {
     
+    // temp fix for the auto-scrolling and resizing banners
+    setupSections();
+
     final section = tabSections[index];
 
     const duration = Duration(milliseconds: 300);
