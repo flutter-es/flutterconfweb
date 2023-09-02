f3475b2b0d19b988da140611d927978c18bfbfed
Roman Jaquez <romanjaquez@Romans-MacBook-Pro.local>
2023-09-01 23:05:55 -0400

    two-column layout for large screens on the speaker card

--- lib/features/speakers/presentation/widgets/speaker_content_window.dart
+++ lib/features/speakers/presentation/widgets/speaker_content_window.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_conf_colombia/features/sessions/presentation/responsiveness/speaker_session_config.dart';
 import 'package:flutter_conf_colombia/features/sessions/presentation/widgets/session_container.dart';
 import 'package:flutter_conf_colombia/features/speakers/data/models/speaker.model.dart';
 import 'package:flutter_conf_colombia/features/speakers/presentation/widgets/speaker_badge.dart';
 import 'package:flutter_conf_colombia/features/speakers/presentation/widgets/speaker_content.dart';
+import 'package:flutter_conf_colombia/features/speakers/responsiveness/session_single_col_layout.dart';
+import 'package:flutter_conf_colombia/features/speakers/responsiveness/session_two_col_layout.dart';
 import 'package:flutter_conf_colombia/styles/styles.dart';
 
 class SpeakerContentWindow extends StatelessWidget {
@@ -16,19 +19,18 @@ class SpeakerContentWindow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final config = SpeakerSessionConfig.getSpeakerSessionConfig(context);
+
     return Material(
       color: Colors.transparent,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: FlutterConfLatamStyles.mediumPadding,
-          child: Column(
-            children: [
-              SpeakerContent(
-                speaker: speaker,
-              ),
-              SessionContainer(speakerId: speaker.id!)
-            ],
-          ),
+      child: Visibility(
+        visible: config.twoColumnLayout,
+        replacement: SpeakerSessionSingleColumnLayout(
+          speaker: speaker,
+        ),
+        child: SpeakerSessionTwoColumnLayout(
+          speaker: speaker,
         ),
       ),
     );
