38a4cdcef9907a1ba9d275f51be1ecbea3b83559
FabianVarela <jfabian.303@gmail.com>
2025-04-20 13:09:36 -0500

    Create bootstrap file

---
+++ lib/bootstrap.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:timezone/data/latest.dart' as tz;
+
+Future<void> bootstrap(
+  FutureOr<Widget> Function() builder,
+  FirebaseOptions options,
+) async {
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+
+  await runZonedGuarded(
+    () async {
+      usePathUrlStrategy();
+
+      await initializeDateFormatting('es_CO');
+      tz.initializeTimeZones();
+
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp(options: options);
+
+      runApp(ProviderScope(child: await builder()));
+    },
+    (error, stack) {
+      if (kDebugMode) log(error.toString(), stackTrace: stack);
+    },
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,10 @@
-import 'dart:js_interop';
-
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_conf_latam/app.dart';
+import 'package:flutter_conf_latam/bootstrap.dart';
 import 'package:flutter_conf_latam/firebase_options.dart';
-import 'package:flutter_conf_latam/l10n/generated/app_localizations.dart';
-import 'package:flutter_conf_latam/l10n/localization_provider.dart';
-import 'package:flutter_conf_latam/routes/app_routes.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:timezone/data/latest.dart' as tz;
-
-@JS('window')
-external JSWindow get window;
-
-extension type JSWindow._(JSObject _) implements JSObject {
-  external int get initTime;
-}
 
 void main() async {
-  await initializeDateFormatting('es_CO');
-
-  tz.initializeTimeZones();
-  WidgetsFlutterBinding.ensureInitialized();
-
-  //usePathUrlStrategy();
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  runApp(ProviderScope(child: FlutterConf()));
-}
-
-class FlutterConf extends ConsumerWidget {
-  FlutterConf({super.key}) {
-    analytics.logAppOpen();
-
-    final initTime = window.initTime;
-    final currentTime = DateTime.now().millisecondsSinceEpoch;
-
-    final diff = (currentTime - initTime) / 1000;
-    analytics.logEvent(name: 'render_time', parameters: {'diff': diff});
-  }
-
-  final analytics = FirebaseAnalytics.instance;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final appLocale = ref.watch(currentLocalizationProvider);
-
-    return MaterialApp.router(
-      title: 'Flutter Conf LATAM',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Poppins',
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          iconTheme: IconThemeData(color: Colors.black),
-          backgroundColor: Colors.white,
-          centerTitle: true,
-        ),
-      ),
-      locale: appLocale,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        ...AppLocalizations.localizationsDelegates,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      routeInformationProvider: AppRoutes.router.routeInformationProvider,
-      routeInformationParser: AppRoutes.router.routeInformationParser,
-      routerDelegate: AppRoutes.router.routerDelegate,
-    );
-  }
+  await bootstrap(
+    () async => FlutterConfApp(),
+    DefaultFirebaseOptions.currentPlatform,
+  );
 }
