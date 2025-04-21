1b93efa007723277db73c12b4be27b82ed8fbf3c
FabianVarela <jfabian.303@gmail.com>
2025-04-21 13:49:54 -0500

    Rename service file and move to core directory

--- lib/features/shared/providers/shared_providers.dart
+++ lib/features/shared/providers/shared_providers.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_conf_latam/services/weblocalstorage.service.dart';
+import 'package:flutter_conf_latam/core/services/web_local_storage_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
