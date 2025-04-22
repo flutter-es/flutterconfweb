bc2c72513733f4f7fa9eef662a010ddb7d933e7e
FabianVarela <jfabian.303@gmail.com>
2025-04-21 21:27:01 -0500

    Move keys to private file

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter_conf_latam/core/routes/app_routes_keys.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_conf_latam/core/routes/app_routes_pages.dart';
 import 'package:flutter_conf_latam/core/widgets/navigation/shell_navigator_page.dart';
 import 'package:flutter_conf_latam/features/cfp/presentation/pages/cfp_page.dart';
@@ -13,74 +13,72 @@ import 'package:flutter_conf_latam/features/sponsors/presentation/pages/sponsors
 import 'package:flutter_conf_latam/features/tickets/presentation/pages/tickets_page.dart';
 import 'package:go_router/go_router.dart';
 
+final _mainRoutesKey = GlobalKey<NavigatorState>(debugLabel: 'root');
+final _tabRoutesKeys = GlobalKey<NavigatorState>(debugLabel: 'tabShell');
+
 class AppRoutes {
   AppRoutes()
     : _router = GoRouter(
         initialLocation: SplashPage.route,
         routerNeglect: true,
-        navigatorKey: mainRoutesKey,
+        navigatorKey: _mainRoutesKey,
         routes: <RouteBase>[
           GoRoute(
             path: SplashPage.route,
-            parentNavigatorKey: mainRoutesKey,
+            parentNavigatorKey: _mainRoutesKey,
             pageBuilder: (_, state) {
               return ScreenPage(key: state.pageKey, child: const SplashPage());
             },
           ),
           ShellRoute(
-            navigatorKey: tabRoutesKeys,
-            pageBuilder: (_, state, child) {
-              return ScreenPage(
-                key: state.pageKey,
-                child: ShellNavigatorPage(child: child),
-              );
-            },
+            navigatorKey: _tabRoutesKeys,
+            builder: (_, __, child) => ShellNavigatorPage(child: child),
             routes: <RouteBase>[
               GoRoute(
                 path: HomePage.route,
-                parentNavigatorKey: tabRoutesKeys,
+                parentNavigatorKey: _tabRoutesKeys,
                 pageBuilder: (_, __) {
                   return const NoTransitionPage(child: HomePage());
                 },
               ),
               GoRoute(
                 path: CFPPage.route,
-                parentNavigatorKey: tabRoutesKeys,
+                parentNavigatorKey: _tabRoutesKeys,
                 pageBuilder: (_, __) {
                   return const NoTransitionPage(child: CFPPage());
                 },
               ),
               GoRoute(
                 path: TicketsPage.route,
-                parentNavigatorKey: tabRoutesKeys,
+                parentNavigatorKey: _tabRoutesKeys,
                 pageBuilder: (_, __) {
                   return const NoTransitionPage(child: TicketsPage());
                 },
               ),
               GoRoute(
                 path: SpeakersPage.route,
-                parentNavigatorKey: tabRoutesKeys,
+                parentNavigatorKey: _tabRoutesKeys,
                 pageBuilder: (_, __) {
                   return const NoTransitionPage(child: SpeakersPage());
                 },
               ),
               GoRoute(
                 path: SponsorsPage.route,
-                parentNavigatorKey: tabRoutesKeys,
+                parentNavigatorKey: _tabRoutesKeys,
                 pageBuilder: (_, __) {
                   return const NoTransitionPage(child: SponsorsPage());
                 },
               ),
               GoRoute(
                 path: SchedulePage.route,
-                parentNavigatorKey: tabRoutesKeys,
+                parentNavigatorKey: _tabRoutesKeys,
                 pageBuilder: (_, __) {
                   return const NoTransitionPage(child: SchedulePage());
                 },
               ),
               GoRoute(
                 path: ContactPage.route,
-                parentNavigatorKey: tabRoutesKeys,
+                parentNavigatorKey: _tabRoutesKeys,
                 pageBuilder: (_, __) {
                   return const NoTransitionPage(child: ContactPage());
                 },
--- lib/core/routes/app_routes_keys.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-final mainRoutesKey = GlobalKey<NavigatorState>(debugLabel: 'root');
-final tabRoutesKeys = GlobalKey<NavigatorState>(debugLabel: 'tabShell');
-final mainScaffoldKey = GlobalKey<ScaffoldState>();
