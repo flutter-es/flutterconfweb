f2e5b50930505ce8bf4dafbcb2c06e1834400990
FabianVarela <jfabian.303@gmail.com>
2025-04-23 23:04:53 -0500

    Modify language button

--- lib/features/navigation/presentation/widgets/language_button.dart
+++ lib/features/navigation/presentation/widgets/language_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_conf_latam/features/navigation/presentation/responsiveness/navigation_responsive_config.dart';
-import 'package:flutter_conf_latam/features/navigation/presentation/widgets/language_item.dart';
 import 'package:flutter_conf_latam/l10n/generated/app_localizations.dart';
 import 'package:flutter_conf_latam/l10n/localization_provider.dart';
 import 'package:flutter_conf_latam/l10n/support_locale.dart';
@@ -13,20 +12,19 @@ class LanguageButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(currentLocalizationProvider);
-    const locals = AppLocalizations.supportedLocales;
+    final languageCode = ref.watch(currentLocalizationProvider).languageCode;
     final uiConfig = NavigationResponsiveConfig.getNavigationConfig(context);
 
     return Container(
-      margin: const EdgeInsets.all(10),
-      width: uiConfig.languageButtonWidth,
       height: 50,
+      width: uiConfig.languageButtonWidth,
+      margin: const EdgeInsets.all(10),
       decoration: const BoxDecoration(
-        color: Color(0xffCBECFF),
+        color: FlutterLatamColors.lightBlue,
         borderRadius: BorderRadius.all(Radius.circular(60)),
       ),
       child: Stack(
-        children: [
+        children: <Widget>[
           FractionallySizedBox(
             widthFactor: 0.5,
             child: Container(
@@ -37,29 +35,61 @@ class LanguageButton extends ConsumerWidget {
               ),
             ),
           ).animate().slideX(
-            begin: state.languageCode == SupportLocale.en.languageCode ? 1 : 0,
-            end: state.languageCode == SupportLocale.en.languageCode ? 0 : 1,
-            duration: 0.seconds,
+            begin: languageCode == SupportLocale.en.languageCode ? 1 : 0,
+            end: languageCode == SupportLocale.en.languageCode ? 0 : 1,
+            duration: 1.seconds,
           ),
-          Center(
-            child: Row(
-              children: [
-                for (final local in locals)
-                  Expanded(
-                    child: LanguageItem(
-                      locale: local,
-                      title: local.languageCode,
-                      isActive: state.languageCode == local.languageCode,
-                      onTap: (Locale selectedLocale) {
-                        ref.read(currentLocalizationProvider.notifier).state =
-                            selectedLocale;
-                      },
-                    ),
+          Row(
+            children: <Widget>[
+              for (final local in AppLocalizations.supportedLocales)
+                Expanded(
+                  child: _LanguageItem(
+                    title: local.languageCode,
+                    isActive: languageCode == local.languageCode,
+                    onTap: () {
+                      ref.read(currentLocalizationProvider.notifier).state =
+                          local;
+                    },
                   ),
-              ],
-            ),
+                ),
+            ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _LanguageItem extends StatelessWidget {
+  const _LanguageItem({
+    required this.title,
+    required this.isActive,
+    required this.onTap,
+  });
+
+  final String title;
+  final bool isActive;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        child: InkWell(
+          onTap: onTap,
+          child: Text(
+            title.toUpperCase(),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: switch (isActive) {
+                true => FlutterLatamColors.white,
+                false => FlutterLatamColors.darkBlue,
+              },
+            ),
+          ),
+        ),
       ),
     );
   }
