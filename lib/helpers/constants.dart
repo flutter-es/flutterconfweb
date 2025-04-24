57d51557a08e89fc82286ef7d156915119caf0fb
FabianVarela <jfabian.303@gmail.com>
2025-04-23 20:49:02 -0500

    Add generated assets and remove constants

--- lib/helpers/constants.dart
+++
@@ -1,5 +0,0 @@
-class Constants {
-  static const String imagesPath = './assets/images';
-  static const String animationsPath = './assets/anims';
-  static const String conferenceEmail = 'flutterconflatam@gmail.com';
-}
---
+++ lib/styles/generated/assets.gen.dart
@@ -0,0 +1,72 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsAnimationsGen {
+  const $AssetsAnimationsGen();
+
+  /// File path: assets/animations/flutter_warning.riv
+  String get flutterWarning => 'assets/animations/flutter_warning.riv';
+
+  /// File path: assets/animations/flutterconflogo.riv
+  String get flutterconflogo => 'assets/animations/flutterconflogo.riv';
+
+  /// File path: assets/animations/flutterdash.riv
+  String get flutterdash => 'assets/animations/flutterdash.riv';
+
+  /// List of all assets
+  List<String> get values => [flutterWarning, flutterconflogo, flutterdash];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/fcl_text_white.svg
+  String get fclTextWhite => 'assets/images/fcl_text_white.svg';
+
+  /// File path: assets/images/flutter_logo_color.svg
+  String get flutterLogoColor => 'assets/images/flutter_logo_color.svg';
+
+  /// File path: assets/images/flutter_logo_white.svg
+  String get flutterLogoWhite => 'assets/images/flutter_logo_white.svg';
+
+  /// File path: assets/images/medellin.jpg
+  String get medellin => 'assets/images/medellin.jpg';
+
+  /// Directory path: assets/images/sponsors
+  $AssetsImagesSponsorsGen get sponsors => const $AssetsImagesSponsorsGen();
+
+  /// File path: assets/images/topbanner.jpg
+  String get topbanner => 'assets/images/topbanner.jpg';
+
+  /// List of all assets
+  List<String> get values => [
+    fclTextWhite,
+    flutterLogoColor,
+    flutterLogoWhite,
+    medellin,
+    topbanner,
+  ];
+}
+
+class $AssetsImagesSponsorsGen {
+  const $AssetsImagesSponsorsGen();
+
+  /// File path: assets/images/sponsors/flutter_logo.svg
+  String get flutterLogo => 'assets/images/sponsors/flutter_logo.svg';
+
+  /// List of all assets
+  List<String> get values => [flutterLogo];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsAnimationsGen animations = $AssetsAnimationsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
