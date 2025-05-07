f42beff90ee6298bd6c3194b0811927ed0a4c020
FabianVarela <jfabian.303@gmail.com>
2025-05-07 12:42:36 -0500

    Remove unused providers

--- lib/features/home/presentation/providers/home_providers.dart
+++ lib/features/home/presentation/providers/home_providers.dart
@@ -1,15 +1,5 @@
-import 'package:flutter_conf_latam/features/home/data/repositories/datelocation_container.repository.dart';
-import 'package:flutter_conf_latam/features/home/data/repositories/event_feature.repository.dart';
 import 'package:flutter_conf_latam/features/home/data/repositories/home_container_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final eventFeaturesRepositoryProvider = Provider((ref) {
-  return EventFeatureRepository(ref);
-});
-
-final eventFeaturesListProvider = Provider((ref) {
-  return ref.watch(eventFeaturesRepositoryProvider).getEventFeatures();
-});
 
 final homeContainerRepositoryProvider = Provider((ref) {
   return HomeContainerRepository(ref);
@@ -17,14 +7,4 @@ final homeContainerRepositoryProvider = Provider((ref) {
 
 final homeContainerDataProvider = Provider((ref) {
   return ref.watch(homeContainerRepositoryProvider).getHomeContainerContent();
-});
-
-final dateLocationRepositoryProvider = Provider((ref) {
-  return DateLocationRepository(ref);
-});
-
-final dateLocationProvider = Provider((ref) {
-  return ref
-      .watch(dateLocationRepositoryProvider)
-      .getDateLocationContainerContent();
 });
