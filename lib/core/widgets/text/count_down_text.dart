cc995d51c228b63f14eebba0a2ffdacdada7de56
FabianVarela <jfabian.303@gmail.com>
2025-05-10 11:22:12 -0500

    User LayoutGrid instead

--- lib/core/widgets/text/count_down_text.dart
+++ lib/core/widgets/text/count_down_text.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_conf_latam/core/responsive/responsive_context_layout.dart';
 import 'package:flutter_conf_latam/core/widgets/text/title_subtitle_text.dart';
+import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 
 class CountDownText extends StatefulWidget {
   const CountDownText({
@@ -38,46 +39,40 @@ class _CountDownTextState extends State<CountDownText> {
 
   @override
   Widget build(BuildContext context) {
-    final items = <Widget>[
-      for (final item in _formatDuration(_duration))
-        TitleSubtitleText(
-          title: (
-            text: item.value,
-            size: switch (context.screenSize) {
-              ScreenSize.extraLarge => 84,
-              ScreenSize.large => 64,
-              ScreenSize.normal || ScreenSize.small => 24,
-            },
+    return LayoutGrid(
+      rowGap: 30,
+      columnGap: 30,
+      columnSizes: switch (context.screenSize) {
+        ScreenSize.extraLarge => [1.fr, 1.fr, 1.fr, 1.fr],
+        _ => [1.fr, 1.fr],
+      },
+      rowSizes: switch (context.screenSize) {
+        ScreenSize.extraLarge => [auto, auto, auto, auto],
+        _ => [auto, auto],
+      },
+      children: <Widget>[
+        for (final item in _formatDuration(_duration))
+          Center(
+            child: TitleSubtitleText(
+              title: (
+                text: item.value,
+                size: switch (context.screenSize) {
+                  ScreenSize.extraLarge => 84,
+                  ScreenSize.large => 64,
+                  ScreenSize.normal || ScreenSize.small => 24,
+                },
+              ),
+              subtitle: (
+                text: item.text,
+                size: switch (context.screenSize) {
+                  ScreenSize.extraLarge || ScreenSize.large => 24,
+                  ScreenSize.normal || ScreenSize.small => 20,
+                },
+              ),
+            ),
           ),
-          subtitle: (
-            text: item.text,
-            size: switch (context.screenSize) {
-              ScreenSize.extraLarge || ScreenSize.large => 24,
-              ScreenSize.normal || ScreenSize.small => 20,
-            },
-          ),
-        ),
-    ];
-
-    return switch (context.screenSize) {
-      ScreenSize.extraLarge => Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: items,
-      ),
-      ScreenSize.large ||
-      ScreenSize.small ||
-      ScreenSize.normal => GridView.count(
-        crossAxisCount: 2,
-        shrinkWrap: true,
-        childAspectRatio: switch (context.screenSize) {
-          ScreenSize.large => 2,
-          ScreenSize.normal || ScreenSize.small => 1.5,
-          _ => 1,
-        },
-        physics: const NeverScrollableScrollPhysics(),
-        children: items,
-      ),
-    };
+      ],
+    );
   }
 
   void _startTimer() {
