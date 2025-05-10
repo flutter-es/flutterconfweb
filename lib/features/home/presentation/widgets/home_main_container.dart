d31dbb6725c694ef3e093cf44c79ae52bf4199eb
FabianVarela <jfabian.303@gmail.com>
2025-05-10 11:22:36 -0500

    Remove SizedBox

--- lib/features/home/presentation/widgets/home_main_container.dart
+++ lib/features/home/presentation/widgets/home_main_container.dart
@@ -12,111 +12,102 @@ class HomeMainContainer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SizedBox(
-      height: switch (context.screenSize) {
-        ScreenSize.extraLarge => 1650,
-        ScreenSize.large => 1500,
-        ScreenSize.normal || ScreenSize.small => 800,
-      },
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: switch (context.screenSize) {
-            ScreenSize.extraLarge => 122,
-            ScreenSize.large => 72,
-            ScreenSize.normal || ScreenSize.small => 28,
-          },
-        ),
-        child: Column(
-          spacing: switch (context.screenSize) {
-            ScreenSize.extraLarge || ScreenSize.large => 96,
-            ScreenSize.normal || ScreenSize.small => 48,
-          },
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(
-                top: switch (context.screenSize) {
-                  ScreenSize.extraLarge || ScreenSize.large => 48,
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: switch (context.screenSize) {
+          ScreenSize.extraLarge => 122,
+          ScreenSize.large => 72,
+          ScreenSize.normal || ScreenSize.small => 28,
+        },
+      ),
+      child: Column(
+        spacing: switch (context.screenSize) {
+          ScreenSize.extraLarge || ScreenSize.large => 96,
+          ScreenSize.normal || ScreenSize.small => 48,
+        },
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              top: switch (context.screenSize) {
+                ScreenSize.extraLarge || ScreenSize.large => 48,
+                ScreenSize.normal || ScreenSize.small => 24,
+              },
+            ),
+            child: TitleSubtitleText(
+              title: (
+                text: 'FlutterConfLatam 2025\nQuito - Ecuador',
+                size: switch (context.screenSize) {
+                  ScreenSize.extraLarge => 64,
+                  ScreenSize.large => 48,
                   ScreenSize.normal || ScreenSize.small => 24,
                 },
               ),
-              child: TitleSubtitleText(
-                title: (
-                  text: 'FlutterConfLatam 2025\nQuito - Ecuador',
-                  size: switch (context.screenSize) {
-                    ScreenSize.extraLarge => 64,
-                    ScreenSize.large => 48,
-                    ScreenSize.normal || ScreenSize.small => 24,
-                  },
-                ),
-                subtitle: (
-                  text: '9 y 10 de Septiembre',
-                  size: switch (context.screenSize) {
-                    ScreenSize.extraLarge => 40,
-                    ScreenSize.large => 36,
-                    ScreenSize.normal || ScreenSize.small => 16,
-                  },
-                ),
-                spacing: switch (context.screenSize) {
-                  ScreenSize.extraLarge || ScreenSize.large => 24,
-                  ScreenSize.normal || ScreenSize.small => 12,
-                },
-              ),
-            ),
-            Center(
-              child: SizedBox.fromSize(
+              subtitle: (
+                text: '9 y 10 de Septiembre',
                 size: switch (context.screenSize) {
-                  ScreenSize.extraLarge => const Size(760, 750),
-                  ScreenSize.large => const Size(520, 513),
-                  ScreenSize.normal || ScreenSize.small => const Size(300, 296),
+                  ScreenSize.extraLarge => 40,
+                  ScreenSize.large => 36,
+                  ScreenSize.normal || ScreenSize.small => 16,
                 },
-                child: DecoratedBox(
-                  decoration: const BoxDecoration(
-                    gradient: RadialGradient(
-                      colors: [
-                        FlutterLatamColors.lightBlue,
-                        FlutterLatamColors.mainBlue,
-                      ],
-                      stops: [0.4, 1],
-                    ),
+              ),
+              spacing: switch (context.screenSize) {
+                ScreenSize.extraLarge || ScreenSize.large => 24,
+                ScreenSize.normal || ScreenSize.small => 12,
+              },
+            ),
+          ),
+          Center(
+            child: SizedBox.fromSize(
+              size: switch (context.screenSize) {
+                ScreenSize.extraLarge => const Size(760, 750),
+                ScreenSize.large => const Size(520, 513),
+                ScreenSize.normal || ScreenSize.small => const Size(300, 296),
+              },
+              child: DecoratedBox(
+                decoration: const BoxDecoration(
+                  gradient: RadialGradient(
+                    colors: [
+                      FlutterLatamColors.lightBlue,
+                      FlutterLatamColors.mainBlue,
+                    ],
+                    stops: [0.4, 1],
                   ),
-                  child: Image.asset(Assets.images.ecuaDash),
                 ),
+                child: Image.asset(Assets.images.ecuaDash),
               ),
             ),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                text: '¡La experiencia Flutter te espera en la ',
-                children: <InlineSpan>[
-                  TextSpan(
-                    text: 'Mitad del Mundo',
-                    style: const TextStyle(
-                      decoration: TextDecoration.underline,
-                      decorationColor: FlutterLatamColors.white,
-                    ),
-                    recognizer: TapGestureRecognizer()..onTap = () {},
+          ),
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              text: '¡La experiencia Flutter te espera en la ',
+              children: <InlineSpan>[
+                TextSpan(
+                  text: 'Mitad del Mundo',
+                  style: const TextStyle(
+                    decoration: TextDecoration.underline,
+                    decorationColor: FlutterLatamColors.white,
                   ),
-                  const TextSpan(text: '!'),
-                ],
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w400,
-                  color: FlutterLatamColors.white,
-                  fontSize: switch (context.screenSize) {
-                    ScreenSize.extraLarge || ScreenSize.large => 40,
-                    ScreenSize.normal || ScreenSize.small => 16,
-                  },
+                  recognizer: TapGestureRecognizer()..onTap = () {},
                 ),
+                const TextSpan(text: '!'),
+              ],
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w400,
+                color: FlutterLatamColors.white,
+                fontSize: switch (context.screenSize) {
+                  ScreenSize.extraLarge || ScreenSize.large => 40,
+                  ScreenSize.normal || ScreenSize.small => 16,
+                },
               ),
             ),
-            Expanded(
-              child: CountDownText(
-                startDate: DateTime.now(),
-                endDate: DateTime(2025, 9, 10),
-              ),
-            ),
-          ],
-        ),
+          ),
+          CountDownText(
+            startDate: DateTime.now(),
+            endDate: DateTime(2025, 9, 10),
+          ),
+        ],
       ),
     );
   }
