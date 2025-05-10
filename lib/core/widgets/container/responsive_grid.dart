5c2c29758f03ec2f7184dcb384b7944f5158c86a
FabianVarela <jfabian.303@gmail.com>
2025-05-10 15:25:57 -0500

    Improve responsive grid widget

--- lib/core/widgets/container/responsive_grid.dart
+++ lib/core/widgets/container/responsive_grid.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_conf_latam/core/responsive/responsive_context_layout.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 
 class ResponsiveGrid extends StatelessWidget {
-  const ResponsiveGrid({required this.children, super.key});
+  const ResponsiveGrid({
+    required this.children,
+    required this.columnSizes,
+    required this.rowSizes,
+    super.key,
+  });
 
   final List<Widget> children;
+  final int columnSizes;
+  final int rowSizes;
 
   @override
   Widget build(BuildContext context) {
     return LayoutGrid(
       rowGap: 30,
       columnGap: 30,
-      autoPlacement: AutoPlacement.rowDense,
-      columnSizes: switch (context.screenSize) {
-        ScreenSize.extraLarge || ScreenSize.large => [1.fr, 1.fr],
-        ScreenSize.normal || ScreenSize.small => [1.fr],
-      },
-      rowSizes: switch (context.screenSize) {
-        ScreenSize.extraLarge || ScreenSize.large => [auto, auto],
-        ScreenSize.normal ||
-        ScreenSize.small => List.generate(children.length, (_) => auto),
-      },
+      columnSizes: List.generate(columnSizes, (_) => 1.fr),
+      rowSizes: List.generate(rowSizes, (_) => auto),
       children: children,
     );
   }
