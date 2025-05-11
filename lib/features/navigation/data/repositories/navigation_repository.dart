37671a07a1b3d1e952a66d8e9d24905631bccc8d
FabianVarela <jfabian.303@gmail.com>
2025-05-11 11:19:07 -0500

    Remove unused navigator

--- lib/features/navigation/data/repositories/navigation_repository.dart
+++ lib/features/navigation/data/repositories/navigation_repository.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_conf_latam/features/home/presentation/pages/home_page.dart';
 import 'package:flutter_conf_latam/features/navigation/data/models/navigation_item_model.dart';
-import 'package:flutter_conf_latam/l10n/localization_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class NavigationRepository {
@@ -9,11 +8,11 @@ class NavigationRepository {
   final Ref ref;
 
   List<NavigationItemModel> getNavigation() {
-    final appLoc = ref.watch(appLocalizationsProvider);
+    // final appLoc = ref.watch(appLocalizationsProvider);
 
     return <NavigationItemModel>[
-      NavigationItemModel(
-        label: appLoc.home,
+      const NavigationItemModel(
+        label: '',
         route: HomePage.route,
         isSelected: true,
         display: false,
