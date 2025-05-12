6c2d7cd88e3df3c2743fdb672b0f64570099ce22
FabianVarela <jfabian.303@gmail.com>
2025-05-12 13:43:48 -0500

    Add new colors

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -8,8 +8,10 @@ class FlutterLatamColors {
   static const Color mediumBlue = Color(0xFF78B7FD);
   static const Color blue = Color(0xFF016EE8);
   static const Color yellow = Color(0xFFFAB92D);
+  static const Color lightYellow = Color(0xFFFFEA96);
   static const Color green = Color(0xFF78B94A);
   static const Color red = Color(0xFFD30D19);
+  static const Color lightRed = Color(0xFFFB8A91);
 
   // Sponsors
 
