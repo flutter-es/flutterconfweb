dd3f0a226ffa72d658362619b99b42510a8ff783
FabianVarela <jfabian.303@gmail.com>
2025-05-16 18:07:43 -0500

    Create reusable widget for set images in a carousel

---
+++ lib/core/widgets/images/carousel_images.dart
@@ -0,0 +1,144 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_conf_latam/styles/colors.dart';
+
+class CarouselImages extends StatefulWidget {
+  const CarouselImages({required this.images, this.aspectRatio = 2, super.key})
+    : assert(images.length >= 1, 'Carousel must have at least one image');
+
+  final List<String> images;
+  final double aspectRatio;
+
+  @override
+  State<CarouselImages> createState() => _CarouselImagesState();
+}
+
+class _CarouselImagesState extends State<CarouselImages> {
+  late final PageController _pageController = PageController();
+
+  int _currentPage = 0;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.images.length > 1) _startAutoScroll();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _pageController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final width = constraints.maxWidth;
+        return Column(
+          spacing: 10,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            SizedBox.fromSize(
+              size: Size(width, width / widget.aspectRatio),
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: widget.images.length,
+                onPageChanged: (page) {
+                  setState(() => _currentPage = page);
+                  _resetTimer();
+                },
+                itemBuilder: (_, index) {
+                  return AspectRatio(
+                    aspectRatio: widget.aspectRatio,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.network(
+                        widget.images[index],
+                        fit: BoxFit.cover,
+                        loadingBuilder: (_, child, loadingProgress) {
+                          if (loadingProgress == null) return child;
+
+                          final value = loadingProgress.cumulativeBytesLoaded;
+                          final total = loadingProgress.expectedTotalBytes;
+
+                          return Center(
+                            child: CircularProgressIndicator(
+                              color: FlutterLatamColors.white,
+                              value: total != null ? value / total : null,
+                            ),
+                          );
+                        },
+                        errorBuilder: (_, __, ___) {
+                          return const Center(
+                            child: Text('Error al cargar la imagen'),
+                          );
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            if (widget.images.length > 1)
+              Row(
+                spacing: 4,
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(widget.images.length, (index) {
+                  return MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                      onTap: () {
+                        if (_pageController.hasClients) _animatePage(index);
+                        _resetTimer();
+                      },
+                      child: SizedBox.square(
+                        dimension: 12,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: switch (_currentPage == index) {
+                              true => FlutterLatamColors.white,
+                              false => FlutterLatamColors.darkBlue,
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _startAutoScroll() {
+    _timer?.cancel();
+    _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
+      if (!mounted && (widget.images.isEmpty || !_pageController.hasClients)) {
+        timer.cancel();
+        return;
+      }
+      _animatePage((_currentPage + 1) % widget.images.length);
+    });
+  }
+
+  void _animatePage(int index) {
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  void _resetTimer() {
+    if (widget.images.length > 1) _startAutoScroll();
+  }
+}
--- lib/features/about/presentation/widgets/about_main.dart
+++ lib/features/about/presentation/widgets/about_main.dart
@@ -1,10 +1,9 @@
-import 'dart:async'; // Added for Timer
-
 import 'package:flutter/material.dart';
 import 'package:flutter_conf_latam/core/responsive/responsive_context_layout.dart';
 import 'package:flutter_conf_latam/core/utils/utils.dart';
 import 'package:flutter_conf_latam/core/widgets/button/fcl_button.dart';
 import 'package:flutter_conf_latam/core/widgets/container/section_container.dart';
+import 'package:flutter_conf_latam/core/widgets/images/carousel_images.dart';
 import 'package:flutter_conf_latam/core/widgets/text/adaptable_text.dart';
 import 'package:flutter_conf_latam/core/widgets/text/title_subtitle_text.dart';
 import 'package:flutter_conf_latam/l10n/localization_provider.dart';
@@ -33,17 +32,7 @@ class AboutMain extends ConsumerWidget {
     return SectionContainer(
       spacing: 48,
       children: <Widget>[
-        ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 1200),
-          child: SizedBox(
-            width: 1196,
-            child: _ImageCarouselWidget(
-              imageUrls: carouselImageUrls,
-              height: 673, // Set specific height for the carousel
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
-        ),
+        CarouselImages(images: carouselImageUrls),
         Column(
           spacing: 24,
           mainAxisSize: MainAxisSize.min,
@@ -131,164 +120,6 @@ class AboutMain extends ConsumerWidget {
             ),
           ],
         ),
-      ],
-    );
-  }
-}
-
-class _ImageCarouselWidget extends StatefulWidget {
-  const _ImageCarouselWidget({
-    required this.imageUrls,
-    required this.height,
-    required this.borderRadius,
-  });
-
-  final List<String> imageUrls;
-  final double height;
-  final BorderRadius borderRadius;
-
-  @override
-  State<_ImageCarouselWidget> createState() => _ImageCarouselWidgetState();
-}
-
-class _ImageCarouselWidgetState extends State<_ImageCarouselWidget> {
-  late PageController _pageController;
-  int _currentPage = 0;
-  Timer? _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController(initialPage: _currentPage);
-    if (widget.imageUrls.length > 1) {
-      _startAutoScroll();
-    }
-  }
-
-  void _startAutoScroll() {
-    _timer?.cancel(); // Cancel any existing timer
-    _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
-      if (!mounted) {
-        timer.cancel();
-        return;
-      }
-      if (widget.imageUrls.isEmpty || !_pageController.hasClients) return;
-
-      final nextPage = (_currentPage + 1) % widget.imageUrls.length;
-      _pageController.animateToPage(
-        nextPage,
-        duration: const Duration(milliseconds: 400),
-        curve: Curves.easeInOut,
-      );
-    });
-  }
-
-  void _resetTimer() {
-    if (widget.imageUrls.length > 1) {
-      _startAutoScroll();
-    }
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (widget.imageUrls.isEmpty) {
-      return SizedBox(
-        height: widget.height,
-        child: const Center(child: Text('No images to display')),
-      );
-    }
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        SizedBox(
-          height: widget.height,
-          child: ClipRRect(
-            borderRadius: widget.borderRadius,
-            child: PageView.builder(
-              controller: _pageController,
-              itemCount: widget.imageUrls.length,
-              itemBuilder: (context, index) {
-                return Image.network(
-                  widget.imageUrls[index],
-                  fit: BoxFit.cover,
-                  loadingBuilder: (
-                    BuildContext context,
-                    Widget child,
-                    ImageChunkEvent? loadingProgress,
-                  ) {
-                    if (loadingProgress == null) return child;
-                    return Center(
-                      child: CircularProgressIndicator(
-                        value:
-                            loadingProgress.expectedTotalBytes != null
-                                ? loadingProgress.cumulativeBytesLoaded /
-                                    loadingProgress.expectedTotalBytes!
-                                : null,
-                      ),
-                    );
-                  },
-                  errorBuilder: (
-                    BuildContext context,
-                    Object error,
-                    StackTrace? stackTrace,
-                  ) {
-                    return const Center(
-                      child: Icon(Icons.error, color: Colors.red, size: 50),
-                    );
-                  },
-                );
-              },
-              onPageChanged: (index) {
-                setState(() {
-                  _currentPage = index;
-                });
-                _resetTimer(); // Reset timer on manual swipe
-              },
-            ),
-          ),
-        ),
-        if (widget.imageUrls.length > 1) ...[
-          const SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(widget.imageUrls.length, (index) {
-              return GestureDetector(
-                onTap: () {
-                  if (_pageController.hasClients) {
-                    _pageController.animateToPage(
-                      index,
-                      duration: const Duration(milliseconds: 400),
-                      curve: Curves.easeInOut,
-                    );
-                  }
-                  _resetTimer();
-                },
-                child: Container(
-                  width: 12,
-                  height: 12,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 4,
-                  ),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color:
-                        _currentPage == index
-                            ? Colors.white
-                            : const Color(0xFF0D4377),
-                  ),
-                ),
-              );
-            }),
-          ),
-        ],
       ],
     );
   }
