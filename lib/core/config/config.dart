a2c85d68a691584227c9cc8ef27c1854b33aaff5
FabianVarela <jfabian.303@gmail.com>
2025-05-17 00:01:09 -0500

    Create config class and inject it

---
+++ lib/core/config/config.dart
@@ -0,0 +1,15 @@
+class Config {
+  String get countryPageUrl => const String.fromEnvironment('COUNTRY_PAGE_URL');
+
+  String get ticketPageUrl => const String.fromEnvironment('TICKET_PAGE_URL');
+
+  String get cfpFormUrl => const String.fromEnvironment('CFP_FORM_URL');
+
+  String get appStoreUrl => const String.fromEnvironment('APP_STORE_URL');
+
+  String get googleAppUrl => const String.fromEnvironment('GOOGLE_APP_URL');
+
+  String get venueMapUrl => const String.fromEnvironment('VENUE_MAP_URL');
+
+  String get contactEmail => const String.fromEnvironment('CONTACT_EMAIL');
+}
--- lib/core/dependencies.dart
+++ lib/core/dependencies.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_conf_latam/core/config/config.dart';
 import 'package:flutter_conf_latam/core/services/web_local_storage_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -14,3 +15,5 @@ final webLocalStorageProvider = Provider((ref) {
     preferences: ref.watch(sharedPrefsInstanceProvider),
   );
 });
+
+final configProvider = Provider((ref) => Config());
