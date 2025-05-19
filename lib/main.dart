0e0438da2e93575e13d384fb86c5f4b753d49c2a
FabianVarela <jfabian.303@gmail.com>
2025-05-19 07:43:41 -0500

    Set current Default Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_conf_latam/app.dart';
 import 'package:flutter_conf_latam/bootstrap.dart';
+import 'package:flutter_conf_latam/firebase_options.dart';
 
 void main() async {
   await bootstrap(
     () async => FlutterConfApp(),
-    // Provide the required FirebaseOptions object as the second argument
-    const FirebaseOptions(
-      apiKey: 'your-api-key',
-      appId: 'your-app-id',
-      messagingSenderId: 'your-messaging-sender-id',
-      projectId: 'your-project-id',
-    ),
+    DefaultFirebaseOptions.currentPlatform,
   );
 }
