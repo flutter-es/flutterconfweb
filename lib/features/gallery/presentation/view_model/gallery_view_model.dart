58aa23c6c9cfad77b8ed9c706676b60bee35f972
FabianVarela <jfabian.303@gmail.com>
2025-06-01 18:02:49 -0500

    Update providers to handle pagination

--- lib/features/gallery/presentation/view_model/gallery_view_model.dart
+++ lib/features/gallery/presentation/view_model/gallery_view_model.dart
@@ -1,6 +1,31 @@
 import 'package:flutter_conf_latam/features/gallery/data/gallery_repository.dart';
+import 'package:flutter_conf_latam/features/gallery/domain/models/gallery_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final galleryProvider = FutureProvider((ref) {
+final galleryDataProvider = FutureProvider((ref) {
   return ref.watch(galleryRepositoryProvider).getGallery();
+});
+
+typedef Pagination = ({int page, int pageSize});
+
+final paginationProvider = StateProvider<Pagination>(
+  (_) => (page: 1, pageSize: 6),
+);
+
+typedef GalleryInfo = ({List<GalleryModel> galleryList, int totalList});
+
+final galleryProvider = FutureProvider<GalleryInfo>((ref) async {
+  final pagination = ref.watch(paginationProvider);
+  final list = await ref.watch(galleryDataProvider.future);
+
+  final startIndex = (pagination.page - 1) * pagination.pageSize;
+  final endIndex = startIndex + pagination.pageSize;
+
+  if (startIndex >= list.length) {
+    return (galleryList: <GalleryModel>[], totalList: 0);
+  }
+  return (
+    galleryList: list.sublist(startIndex, endIndex.clamp(0, list.length)),
+    totalList: list.length,
+  );
 });
