159adc2dd52bc424534709a40e1e14b7978d853a
FabianVarela <jfabian.303@gmail.com>
2025-06-02 16:47:15 -0500

    Add reusable container and set when loading option

--- lib/features/gallery/presentation/widgets/gallery_list.dart
+++ lib/features/gallery/presentation/widgets/gallery_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter_conf_latam/core/responsive/responsive_context_layout.dar
 import 'package:flutter_conf_latam/core/widgets/container/error_container.dart';
 import 'package:flutter_conf_latam/core/widgets/container/pagination_container.dart';
 import 'package:flutter_conf_latam/core/widgets/container/responsive_grid.dart';
+import 'package:flutter_conf_latam/core/widgets/container/shimmer_container.dart';
 import 'package:flutter_conf_latam/core/widgets/images/single_image.dart';
 import 'package:flutter_conf_latam/features/gallery/presentation/view_model/gallery_view_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -15,44 +16,66 @@ class GalleryList extends ConsumerWidget {
     final paginationInfo = ref.watch(paginationProvider);
     final galleryList = ref.watch(galleryProvider);
 
-    return galleryList.maybeWhen(
-      data: (data) {
-        return PaginationContainer(
-          totalSize: data.totalList,
-          pageSize: paginationInfo.pageSize,
-          currentPage: paginationInfo.page,
-          onChangedPage: (value) {
-            ref.read(paginationProvider.notifier).update((state) {
-              return (page: value, pageSize: state.pageSize);
-            });
-          },
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: switch (context.screenSize) {
-                ScreenSize.extraLarge => 122,
-                ScreenSize.large => 72,
-                ScreenSize.normal || ScreenSize.small => 28,
-              },
-            ),
-            child: ResponsiveGrid(
-              columnSizes: switch (context.screenSize) {
-                ScreenSize.extraLarge => 3,
-                ScreenSize.large => 2,
-                _ => 1,
-              },
-              rowSizes: data.galleryList.length,
-              children: <Widget>[
-                for (final item in data.galleryList)
-                  Center(child: SingleImage(imageUrl: item.imageUrl)),
-              ],
-            ),
-          ),
-        );
-      },
+    return galleryList.when(
+      data: (data) => PaginationContainer(
+        totalSize: data.totalList,
+        pageSize: paginationInfo.pageSize,
+        currentPage: paginationInfo.page,
+        onChangedPage: (value) {
+          ref.read(paginationProvider.notifier).update((state) {
+            return (page: value, pageSize: state.pageSize);
+          });
+        },
+        child: _GalleryListContainer(
+          children: <Widget>[
+            for (final item in data.galleryList)
+              Center(child: SingleImage(imageUrl: item.imageUrl)),
+          ],
+        ),
+      ),
+      loading: () => Shimmer(
+        child: _GalleryListContainer(
+          children: List.generate(9, (_) {
+            return const Center(
+              child: ShimmerLoading(
+                isLoading: true,
+                child: SingleImageContainer(),
+              ),
+            );
+          }),
+        ),
+      ),
       error: (_, _) {
         return ErrorContainer(onRetry: () => ref.invalidate(galleryProvider));
       },
-      orElse: Offstage.new,
+    );
+  }
+}
+
+class _GalleryListContainer extends StatelessWidget {
+  const _GalleryListContainer({required this.children});
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: switch (context.screenSize) {
+          ScreenSize.extraLarge => 122,
+          ScreenSize.large => 72,
+          ScreenSize.normal || ScreenSize.small => 28,
+        },
+      ),
+      child: ResponsiveGrid(
+        columnSizes: switch (context.screenSize) {
+          ScreenSize.extraLarge => 3,
+          ScreenSize.large => 2,
+          _ => 1,
+        },
+        rowSizes: children.length,
+        children: children,
+      ),
     );
   }
 }
