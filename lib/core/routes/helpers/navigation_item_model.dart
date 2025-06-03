7c421d7edf07c371d218622e0d6af26eb6d2737d
FabianVarela <jfabian.303@gmail.com>
2025-06-02 20:49:22 -0500

    Add submenu property and validate in view model

--- lib/core/routes/helpers/navigation_item_model.dart
+++ lib/core/routes/helpers/navigation_item_model.dart
@@ -3,25 +3,54 @@ import 'package:equatable/equatable.dart';
 class NavigationItemModel extends Equatable {
   const NavigationItemModel({
     required this.label,
-    required this.route,
+    this.route,
     this.isSelected = false,
     this.visible = true,
+    this.subMenus,
   });
 
   final String label;
-  final String route;
+  final String? route;
   final bool isSelected;
   final bool visible;
+  final List<SubNavigationItemModel>? subMenus;
 
-  NavigationItemModel copyWith({bool? isSelected}) {
+  NavigationItemModel copyWith({
+    bool? isSelected,
+    List<SubNavigationItemModel>? subMenus,
+  }) {
     return NavigationItemModel(
       label: label,
       route: route,
       isSelected: isSelected ?? this.isSelected,
       visible: visible,
+      subMenus: subMenus ?? this.subMenus,
     );
   }
 
   @override
-  List<Object?> get props => [route, isSelected, visible];
+  List<Object?> get props => [route, isSelected, visible, subMenus];
+}
+
+class SubNavigationItemModel extends Equatable {
+  const SubNavigationItemModel({
+    required this.label,
+    required this.route,
+    this.isSelected = false,
+  });
+
+  final String label;
+  final String route;
+  final bool isSelected;
+
+  SubNavigationItemModel copyWith({bool? isSelected}) {
+    return SubNavigationItemModel(
+      label: label,
+      route: route,
+      isSelected: isSelected ?? this.isSelected,
+    );
+  }
+
+  @override
+  List<Object?> get props => [route, isSelected];
 }
--- lib/core/routes/helpers/navigation_view_model.dart
+++ lib/core/routes/helpers/navigation_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter_conf_latam/core/dependencies.dart';
 import 'package:flutter_conf_latam/core/routes/app_route_path.dart';
 import 'package:flutter_conf_latam/core/routes/helpers/navigation_item_model.dart';
@@ -15,8 +16,13 @@ class NavigationViewModel extends Notifier<List<NavigationItemModel>> {
         isSelected: true,
       ),
       NavigationItemModel(
-        label: l10n.menuVenueText,
-        route: '/${AppRoutePath.venue.pathName}',
+        label: l10n.menuAboutText,
+        subMenus: <SubNavigationItemModel>[
+          SubNavigationItemModel(
+            label: l10n.menuVenueText,
+            route: '/${AppRoutePath.venue.pathName}',
+          ),
+        ],
       ),
       NavigationItemModel(
         label: l10n.menuGalleryText,
@@ -30,7 +36,23 @@ class NavigationViewModel extends Notifier<List<NavigationItemModel>> {
 
     if (_selectedNav.isEmpty) return navigationItemList;
     return navigationItemList.map((item) {
-      if (item.route == _selectedNav) return item.copyWith(isSelected: true);
+      if (item.route != null) {
+        if (item.route == _selectedNav) return item.copyWith(isSelected: true);
+      } else if (item.subMenus != null) {
+        final subMenu = item.subMenus!.firstWhereOrNull(
+          (item) => item.route == _selectedNav,
+        );
+        if (subMenu != null) {
+          final newSubmenu = subMenu.copyWith(isSelected: true);
+          final index = item.subMenus!.indexOf(subMenu);
+
+          final newSubmenus = [...item.subMenus!]
+            ..removeAt(index)
+            ..insert(index, newSubmenu);
+
+          return item.copyWith(isSelected: true, subMenus: newSubmenus);
+        }
+      }
       return item;
     }).toList();
   }
@@ -45,19 +67,36 @@ class NavigationViewModel extends Notifier<List<NavigationItemModel>> {
 
   void selectNavItemFromRoute(String route) {
     selectNavItem(
-      state.firstWhere(
-        (item) => item.route == route,
-        orElse: () => state.first,
-      ),
+      state.firstWhere((item) {
+        if (item.route != null) {
+          return item.route == route;
+        } else if (item.subMenus != null) {
+          return item.subMenus!.where((item) => item.route == route).isNotEmpty;
+        }
+        return false;
+      }, orElse: () => state.first),
     );
   }
 
   void selectNavItem(NavigationItemModel item) {
     state = [
       for (final element in state)
-        element.copyWith(isSelected: item == element),
+        element.copyWith(
+          isSelected: item.route == element.route,
+          subMenus: item.route != element.route
+              ? item.subMenus?.map((item) {
+                  return item.copyWith(isSelected: false);
+                }).toList()
+              : item.subMenus,
+        ),
     ];
-    ref.read(webLocalStorageProvider).storeSelectedNav(item.route);
+
+    final currentRoute =
+        item.route ??
+        item.subMenus?.firstWhereOrNull((item) => item.isSelected)?.route;
+
+    if (currentRoute == null) return;
+    ref.read(webLocalStorageProvider).storeSelectedNav(currentRoute);
   }
 
   String get _selectedNav => ref.read(webLocalStorageProvider).getSelectedNav();
