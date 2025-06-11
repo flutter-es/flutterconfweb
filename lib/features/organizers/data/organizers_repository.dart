397cfb971b6b286d16a30e3e15a46e77ea033fb6
FabianVarela <jfabian.303@gmail.com>
2025-06-10 19:49:46 -0500

    Add method to get organizers

--- lib/features/organizers/data/organizers_repository.dart
+++ lib/features/organizers/data/organizers_repository.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_conf_latam/core/dependencies.dart';
 import 'package:flutter_conf_latam/features/organizers/domain/models/communities/communities_model.dart';
+import 'package:flutter_conf_latam/features/organizers/domain/models/organizers/organizers_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class OrganizersRepository {
@@ -8,7 +9,20 @@ class OrganizersRepository {
 
   final FirebaseFirestore database;
 
+  static const _organizersCollection = 'organizers';
   static const _communitiesCollection = 'communities';
+
+  Future<List<OrganizersModel>> getOrganizers() async {
+    final data = await database
+        .collection(_organizersCollection)
+        .where('is_visible', isEqualTo: true)
+        .get();
+
+    return [
+      for (final item in data.docs)
+        OrganizersModel.fromJson({'id': item.id, ...item.data()}),
+    ];
+  }
 
   Future<List<CommunitiesModel>> getCommunities() async {
     final data = await database.collection(_communitiesCollection).get();
