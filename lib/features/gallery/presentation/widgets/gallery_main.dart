7380a768da8bd6284b82afb476e883295db0cf7a
FabianVarela <jfabian.303@gmail.com>
2025-06-11 20:10:30 -0500

    Become in stateless and set KeepAliveContainer

--- lib/features/gallery/presentation/widgets/gallery_main.dart
+++ lib/features/gallery/presentation/widgets/gallery_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_conf_latam/core/responsive/responsive_context_layout.dart';
+import 'package:flutter_conf_latam/core/widgets/container/keep_alive_container.dart';
 import 'package:flutter_conf_latam/core/widgets/container/section_container.dart';
 import 'package:flutter_conf_latam/core/widgets/images/carousel_images.dart';
 import 'package:flutter_conf_latam/core/widgets/text/title_subtitle_text.dart';
@@ -7,19 +8,11 @@ import 'package:flutter_conf_latam/l10n/localization_provider.dart';
 import 'package:flutter_conf_latam/styles/generated/assets.gen.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class GalleryMain extends ConsumerStatefulWidget {
+class GalleryMain extends ConsumerWidget {
   const GalleryMain({super.key});
 
   @override
-  ConsumerState<GalleryMain> createState() => _GalleryMainState();
-}
-
-class _GalleryMainState extends ConsumerState<GalleryMain>
-    with AutomaticKeepAliveClientMixin {
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-
+  Widget build(BuildContext context, WidgetRef ref) {
     final l10n = ref.watch(appLocalizationsProvider);
     final carouselImageUrls = <String>[
       Assets.images.gallery.one,
@@ -27,32 +20,31 @@ class _GalleryMainState extends ConsumerState<GalleryMain>
       Assets.images.gallery.three,
     ];
 
-    return SectionContainer(
-      spacing: 48,
-      children: <Widget>[
-        CarouselImages(images: carouselImageUrls),
-        TitleSubtitleText(
-          title: (
-            text: l10n.galleryMainTitle,
-            size: switch (context.screenSize) {
-              ScreenSize.extraLarge => 64,
-              ScreenSize.large => 48,
-              ScreenSize.normal || ScreenSize.small => 24,
-            },
+    return KeepAliveContainer(
+      child: SectionContainer(
+        spacing: 48,
+        children: <Widget>[
+          CarouselImages(images: carouselImageUrls),
+          TitleSubtitleText(
+            title: (
+              text: l10n.galleryMainTitle,
+              size: switch (context.screenSize) {
+                ScreenSize.extraLarge => 64,
+                ScreenSize.large => 48,
+                ScreenSize.normal || ScreenSize.small => 24,
+              },
+            ),
+            subtitle: (
+              text: l10n.galleryMainMessage,
+              size: switch (context.screenSize) {
+                ScreenSize.extraLarge || ScreenSize.large => 24,
+                ScreenSize.normal || ScreenSize.small => 16,
+              },
+            ),
+            textAlign: TextAlign.start,
           ),
-          subtitle: (
-            text: l10n.galleryMainMessage,
-            size: switch (context.screenSize) {
-              ScreenSize.extraLarge || ScreenSize.large => 24,
-              ScreenSize.normal || ScreenSize.small => 16,
-            },
-          ),
-          textAlign: TextAlign.start,
-        ),
-      ],
+        ],
+      ),
     );
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
--- lib/features/venue/presentation/widgets/venue_main.dart
+++ lib/features/venue/presentation/widgets/venue_main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_conf_latam/core/dependencies.dart';
 import 'package:flutter_conf_latam/core/responsive/responsive_context_layout.dart';
 import 'package:flutter_conf_latam/core/utils/utils.dart';
 import 'package:flutter_conf_latam/core/widgets/button/fcl_button.dart';
+import 'package:flutter_conf_latam/core/widgets/container/keep_alive_container.dart';
 import 'package:flutter_conf_latam/core/widgets/container/section_container.dart';
 import 'package:flutter_conf_latam/core/widgets/images/carousel_images.dart';
 import 'package:flutter_conf_latam/core/widgets/text/adaptable_text.dart';
@@ -13,19 +14,11 @@ import 'package:flutter_conf_latam/styles/generated/assets.gen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class VenueMain extends ConsumerStatefulWidget {
+class VenueMain extends ConsumerWidget {
   const VenueMain({super.key});
 
   @override
-  ConsumerState<VenueMain> createState() => _VenueMainState();
-}
-
-class _VenueMainState extends ConsumerState<VenueMain>
-    with AutomaticKeepAliveClientMixin {
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-
+  Widget build(BuildContext context, WidgetRef ref) {
     final l10n = ref.watch(appLocalizationsProvider);
     final config = ref.watch(configProvider);
 
@@ -40,101 +33,101 @@ class _VenueMainState extends ConsumerState<VenueMain>
       (text: l10n.aboutVenueCapacity, icon: Assets.images.icons.people),
     ];
 
-    return SectionContainer(
-      spacing: 48,
-      children: <Widget>[
-        CarouselImages(images: carouselImageUrls),
-        Column(
-          spacing: 24,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TitleSubtitleText(
-              title: (
-                text: l10n.aboutVenueName,
-                size: switch (context.screenSize) {
-                  ScreenSize.extraLarge => 64,
-                  ScreenSize.large => 48,
-                  ScreenSize.normal || ScreenSize.small => 24,
-                },
-              ),
-              subtitle: (
-                text: l10n.aboutVenueDescription,
-                size: switch (context.screenSize) {
-                  ScreenSize.extraLarge || ScreenSize.large => 24,
-                  ScreenSize.normal || ScreenSize.small => 16,
-                },
-              ),
-              textAlign: TextAlign.start,
-            ),
-            Flex(
-              spacing: 24,
-              crossAxisAlignment: switch (context.screenSize) {
-                ScreenSize.extraLarge => CrossAxisAlignment.center,
-                _ => CrossAxisAlignment.start,
-              },
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              direction: switch (context.screenSize) {
-                ScreenSize.extraLarge => Axis.horizontal,
-                _ => Axis.vertical,
-              },
-              children: <Widget>[
-                Column(
-                  spacing: 16,
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    for (final item in venueInfoList)
-                      Row(
-                        spacing: 10,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          SizedBox.square(
-                            dimension: switch (context.screenSize) {
-                              ScreenSize.extraLarge || ScreenSize.large => 32,
-                              ScreenSize.normal || ScreenSize.small => 24,
-                            },
-                            child: SvgPicture.asset(item.icon),
-                          ),
-                          AdaptableText(
-                            item.text,
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontSize: switch (context.screenSize) {
-                                ScreenSize.extraLarge || ScreenSize.large => 24,
-                                ScreenSize.normal || ScreenSize.small => 16,
-                              },
-                              fontWeight: FontWeight.w400,
-                              color: FlutterLatamColors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                  ],
-                ),
-                FclButton.primary(
-                  label: l10n.aboutVenueHowToArrive,
-                  buttonSize: switch (context.screenSize) {
-                    ScreenSize.normal || ScreenSize.small => ButtonSize.small,
-                    _ => ButtonSize.large,
+    return KeepAliveContainer(
+      child: SectionContainer(
+        spacing: 48,
+        children: <Widget>[
+          CarouselImages(images: carouselImageUrls),
+          Column(
+            spacing: 24,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TitleSubtitleText(
+                title: (
+                  text: l10n.aboutVenueName,
+                  size: switch (context.screenSize) {
+                    ScreenSize.extraLarge => 64,
+                    ScreenSize.large => 48,
+                    ScreenSize.normal || ScreenSize.small => 24,
                   },
-                  icon: SizedBox.square(
-                    dimension: switch (context.screenSize) {
-                      ScreenSize.extraLarge || ScreenSize.large => 24,
-                      ScreenSize.normal || ScreenSize.small => 20,
-                    },
-                    child: SvgPicture.asset(Assets.images.icons.arrowCurve),
-                  ),
-                  onPressed: () => Utils.launchUrlLink(config.venueMapUrl),
                 ),
-              ],
-            ),
-          ],
-        ),
-      ],
+                subtitle: (
+                  text: l10n.aboutVenueDescription,
+                  size: switch (context.screenSize) {
+                    ScreenSize.extraLarge || ScreenSize.large => 24,
+                    ScreenSize.normal || ScreenSize.small => 16,
+                  },
+                ),
+                textAlign: TextAlign.start,
+              ),
+              Flex(
+                spacing: 24,
+                crossAxisAlignment: switch (context.screenSize) {
+                  ScreenSize.extraLarge => CrossAxisAlignment.center,
+                  _ => CrossAxisAlignment.start,
+                },
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                direction: switch (context.screenSize) {
+                  ScreenSize.extraLarge => Axis.horizontal,
+                  _ => Axis.vertical,
+                },
+                children: <Widget>[
+                  Column(
+                    spacing: 16,
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      for (final item in venueInfoList)
+                        Row(
+                          spacing: 10,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            SizedBox.square(
+                              dimension: switch (context.screenSize) {
+                                ScreenSize.extraLarge || ScreenSize.large => 32,
+                                ScreenSize.normal || ScreenSize.small => 24,
+                              },
+                              child: SvgPicture.asset(item.icon),
+                            ),
+                            AdaptableText(
+                              item.text,
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: switch (context.screenSize) {
+                                  ScreenSize.extraLarge ||
+                                  ScreenSize.large => 24,
+                                  ScreenSize.normal || ScreenSize.small => 16,
+                                },
+                                fontWeight: FontWeight.w400,
+                                color: FlutterLatamColors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
+                  FclButton.primary(
+                    label: l10n.aboutVenueHowToArrive,
+                    buttonSize: switch (context.screenSize) {
+                      ScreenSize.normal || ScreenSize.small => ButtonSize.small,
+                      _ => ButtonSize.large,
+                    },
+                    icon: SizedBox.square(
+                      dimension: switch (context.screenSize) {
+                        ScreenSize.extraLarge || ScreenSize.large => 24,
+                        ScreenSize.normal || ScreenSize.small => 20,
+                      },
+                      child: SvgPicture.asset(Assets.images.icons.arrowCurve),
+                    ),
+                    onPressed: () => Utils.launchUrlLink(config.venueMapUrl),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
