903d1b19d30f9d5563001507c0e8fe160915b79d
FabianVarela <jfabian.303@gmail.com>
2025-06-11 20:11:11 -0500

    Add organizers_people.dart section

--- lib/features/organizers/presentation/pages/organizers_page.dart
+++ lib/features/organizers/presentation/pages/organizers_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_conf_latam/core/widgets/container/footer.dart';
 import 'package:flutter_conf_latam/features/organizers/presentation/widgets/organizers_communities.dart';
+import 'package:flutter_conf_latam/features/organizers/presentation/widgets/organizers_people.dart';
 
 class OrganizersPage extends StatefulWidget {
   const OrganizersPage({super.key});
@@ -25,6 +26,7 @@ class _OrganizersPageState extends State<OrganizersPage> {
       slivers: <Widget>[
         SliverList(
           delegate: SliverChildListDelegate([
+            const OrganizersPeople(),
             const OrganizersCommunities(),
             const Footer(),
           ]),
