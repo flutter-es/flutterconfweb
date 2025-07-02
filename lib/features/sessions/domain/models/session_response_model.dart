87d074023bde746903350b47f818ade45da8cb9a
FabianVarela <jfabian.303@gmail.com>
2025-07-01 19:48:30 -0500

    Create model to handle sessions and add extension methods

---
+++ lib/features/sessions/domain/models/session_response_model.dart
@@ -0,0 +1,119 @@
+import 'package:intl/intl.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'session_response_model.g.dart';
+
+@JsonSerializable(createToJson: false)
+class SessionResponseModel {
+  SessionResponseModel({
+    required this.id,
+    required this.day,
+    required this.slots,
+  });
+
+  factory SessionResponseModel.fromJson(Map<String, dynamic> json) =>
+      _$SessionResponseModelFromJson(json);
+
+  final String id;
+  final int day;
+  final List<SessionSlotModel> slots;
+}
+
+@JsonSerializable(createToJson: false)
+class SessionSlotModel {
+  SessionSlotModel({
+    required this.id,
+    required this.name,
+    required this.sessions,
+  });
+
+  factory SessionSlotModel.fromJson(Map<String, dynamic> json) =>
+      _$SessionSlotModelFromJson(json);
+
+  final String id;
+  final String name;
+  final List<SessionTrackModel> sessions;
+}
+
+@JsonSerializable(createToJson: false)
+class SessionTrackModel {
+  SessionTrackModel({
+    required this.id,
+    required this.title,
+    required this.type,
+    required this.track,
+    required this.speakers,
+    required this.tags,
+    required this.startDate,
+    required this.endDate,
+    this.requirements,
+  });
+
+  factory SessionTrackModel.fromJson(Map<String, dynamic> json) =>
+      _$SessionTrackModelFromJson(json);
+
+  final String id;
+  final String title;
+  final SessionType type;
+  final int track;
+  final List<SessionSpeakerModel> speakers;
+  final List<String> tags;
+  final DateTime startDate;
+  final DateTime endDate;
+  final List<String>? requirements;
+}
+
+@JsonSerializable(createToJson: false)
+class SessionSpeakerModel {
+  SessionSpeakerModel({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  factory SessionSpeakerModel.fromJson(Map<String, dynamic> json) =>
+      _$SessionSpeakerModelFromJson(json);
+
+  final int id;
+  final String name;
+  final String imageUrl;
+}
+
+@JsonEnum()
+enum SessionType {
+  register,
+  keynote,
+  breaks,
+  lighting,
+  session,
+  workshop,
+  finish,
+}
+
+typedef SlotSession = ({String slotId, SessionType type});
+typedef MapSlotSessions = Map<SlotSession, List<SessionTrackModel>>;
+
+extension SessionSlotModelX on SessionSlotModel {
+  MapSlotSessions get sessionSlots {
+    final sessionSlots = <SlotSession, List<SessionTrackModel>>{};
+    for (final session in sessions) {
+      final dateString = DateFormat('HH_mm').format(session.startDate);
+      final key = (slotId: '${id}_$dateString', type: session.type);
+
+      sessionSlots[key] = [...(sessionSlots[key] ?? []), session];
+    }
+    return sessionSlots;
+  }
+}
+
+extension MapSlotSessionsX on MapSlotSessions {
+  List<SessionTrackModel> get otherSessions => <SessionTrackModel>[
+    for (final item in entries)
+      if (item.key.type != SessionType.workshop) ...item.value,
+  ];
+
+  List<SessionTrackModel> get workshopSessions => <SessionTrackModel>[
+    for (final item in entries)
+      if (item.key.type == SessionType.workshop) ...item.value,
+  ];
+}
---
+++ lib/features/sessions/domain/models/session_response_model.g.dart
@@ -0,0 +1,60 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'session_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SessionResponseModel _$SessionResponseModelFromJson(
+  Map<String, dynamic> json,
+) => SessionResponseModel(
+  id: json['id'] as String,
+  day: (json['day'] as num).toInt(),
+  slots: (json['slots'] as List<dynamic>)
+      .map((e) => SessionSlotModel.fromJson(e as Map<String, dynamic>))
+      .toList(),
+);
+
+SessionSlotModel _$SessionSlotModelFromJson(Map<String, dynamic> json) =>
+    SessionSlotModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      sessions: (json['sessions'] as List<dynamic>)
+          .map((e) => SessionTrackModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+SessionTrackModel _$SessionTrackModelFromJson(Map<String, dynamic> json) =>
+    SessionTrackModel(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      type: $enumDecode(_$SessionTypeEnumMap, json['type']),
+      track: (json['track'] as num).toInt(),
+      speakers: (json['speakers'] as List<dynamic>)
+          .map((e) => SessionSpeakerModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      tags: (json['tags'] as List<dynamic>).map((e) => e as String).toList(),
+      startDate: DateTime.parse(json['startDate'] as String),
+      endDate: DateTime.parse(json['endDate'] as String),
+      requirements: (json['requirements'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+    );
+
+const _$SessionTypeEnumMap = {
+  SessionType.register: 'register',
+  SessionType.keynote: 'keynote',
+  SessionType.breaks: 'breaks',
+  SessionType.lighting: 'lighting',
+  SessionType.session: 'session',
+  SessionType.workshop: 'workshop',
+  SessionType.finish: 'finish',
+};
+
+SessionSpeakerModel _$SessionSpeakerModelFromJson(Map<String, dynamic> json) =>
+    SessionSpeakerModel(
+      id: (json['id'] as num).toInt(),
+      name: json['name'] as String,
+      imageUrl: json['imageUrl'] as String,
+    );
