3103c9a4792c63bf95b1e5efdfbc94da525f0ccf
FabianVarela <jfabian.303@gmail.com>
2025-07-06 14:53:12 -0500

    Create typography.dart class

---
+++ lib/styles/typography.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class FclTypography {
+  FclTypography._();
+
+  static const _headingFontFamily = 'Recoleta';
+  static const _bodyFamily = 'Poppins';
+
+  static const _h1Size = 64.0;
+
+  static TextStyle h1 = const TextStyle(
+    fontFamily: _headingFontFamily,
+    fontSize: _h1Size,
+  );
+
+  static const _h2Size = 48.0;
+
+  static TextStyle h2 = const TextStyle(
+    fontFamily: _headingFontFamily,
+    fontSize: _h2Size,
+  );
+
+  static const _h3Size = 32.0;
+
+  static TextStyle h3 = const TextStyle(
+    fontFamily: _headingFontFamily,
+    fontSize: _h3Size,
+  );
+
+  static const _h4Size = 24.0;
+
+  static TextStyle h4 = const TextStyle(
+    fontFamily: _headingFontFamily,
+    fontSize: _h4Size,
+  );
+
+  static const _subH1Size = 40.0;
+
+  static TextStyle subH1 = const TextStyle(
+    fontFamily: _bodyFamily,
+    fontSize: _subH1Size,
+  );
+
+  static const _subH2Size = 18.0;
+
+  static TextStyle subH2 = const TextStyle(
+    fontFamily: _bodyFamily,
+    fontSize: _subH2Size,
+  );
+
+  static const _subH3Size = 18.0;
+
+  static TextStyle subH3 = const TextStyle(
+    fontFamily: _bodyFamily,
+    fontSize: _subH3Size,
+  );
+
+  static const _buttonNormalSize = 20.0;
+
+  static TextStyle buttonNormal = const TextStyle(
+    fontFamily: _bodyFamily,
+    fontSize: _buttonNormalSize,
+  );
+
+  static const _buttonSmallSize = 16.0;
+
+  static TextStyle buttonSmall = const TextStyle(
+    fontFamily: _bodyFamily,
+    fontSize: _buttonSmallSize,
+  );
+
+  static const _body1Size = 24.0;
+
+  static TextStyle body1 = const TextStyle(
+    fontFamily: _bodyFamily,
+    fontSize: _body1Size,
+  );
+
+  static const _body2Size = 18.0;
+
+  static TextStyle body2 = const TextStyle(
+    fontFamily: _bodyFamily,
+    fontSize: _body2Size,
+  );
+
+  static const _body3Size = 16.0;
+
+  static TextStyle body3 = const TextStyle(
+    fontFamily: _bodyFamily,
+    fontSize: _body3Size,
+  );
+
+  static const _body4Size = 14.0;
+
+  static TextStyle body4 = const TextStyle(
+    fontFamily: _bodyFamily,
+    fontSize: _body4Size,
+  );
+
+  static const _captionSize = 12.0;
+
+  static TextStyle caption = const TextStyle(
+    fontFamily: _bodyFamily,
+    fontSize: _captionSize,
+  );
+}
