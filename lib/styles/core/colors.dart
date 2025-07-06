446faef10edd15c209ddcef3d3118cf823c09631
FabianVarela <jfabian.303@gmail.com>
2025-07-06 17:38:30 -0500

    Update colors

--- lib/styles/core/colors.dart
+++ lib/styles/core/colors.dart
@@ -5,8 +5,8 @@ class FlutterLatamColors {
   static const Color black = Color(0xFF000000);
   static const Color darkBlue = Color(0xFF0D4377);
   static const Color mainBlue = Color(0xFF042B59);
-  static const Color mediumBlue = Color(0xFF78B7FD);
   static const Color blue = Color(0xFF016EE8);
+  static const Color mediumBlue = Color(0xFF78B7FD);
   static const Color yellow = Color(0xFFFAB92D);
   static const Color lightYellow = Color(0xFFFFEA96);
   static const Color green = Color(0xFF78B94A);
@@ -15,7 +15,7 @@ class FlutterLatamColors {
   static const Color mediumRed = Color(0xFFCA4D4F);
   static const Color lightRed = Color(0xFFFB8A91);
   static const Color purple = Color(0xFFB86EEE);
+  static const Color pink = Color(0xFFF779E2);
   static const Color silver = Color(0xFFBABABA);
   static const Color grey = Color(0xFF9E9E9E);
-  static const Color pink = Color(0xFFF779E2);
 }
