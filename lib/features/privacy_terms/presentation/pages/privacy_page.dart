0693aa1f150feb0dd5bcb2645bea38082b21c5b3
FabianVarela <jfabian.303@gmail.com>
2025-07-21 17:15:59 -0600

    Replace by private widget

--- lib/features/privacy_terms/presentation/pages/privacy_page.dart
+++ lib/features/privacy_terms/presentation/pages/privacy_page.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_conf_latam/core/widgets/container/footer.dart';
-import 'package:flutter_conf_latam/features/privacy_terms/presentation/widgets/privacy_main.dart';
+import 'package:flutter_conf_latam/features/privacy_terms/presentation/view_model/privacy_terms_view_model.dart';
+import 'package:flutter_conf_latam/features/privacy_terms/presentation/widgets/markdown_container.dart';
+import 'package:flutter_conf_latam/l10n/localization_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class PrivacyPage extends ConsumerStatefulWidget {
@@ -26,11 +28,26 @@ class _PrivacyPageState extends ConsumerState<PrivacyPage> {
       slivers: <Widget>[
         SliverList(
           delegate: SliverChildListDelegate([
-            const TermsMain(),
+            const _PrivacyMain(),
             const Footer(),
           ]),
         ),
       ],
+    );
+  }
+}
+
+class _PrivacyMain extends ConsumerWidget {
+  const _PrivacyMain();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = ref.watch(appLocalizationsProvider);
+    final termsData = ref.watch(termsProvider(Locale(l10n.localeName)));
+
+    return termsData.maybeWhen(
+      data: (data) => MarkdownContainer(markdownData: data),
+      orElse: Offstage.new,
     );
   }
 }
