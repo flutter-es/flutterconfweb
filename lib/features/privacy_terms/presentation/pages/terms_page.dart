8e453c37859693123987676be58c6cf59b875b85
FabianVarela <jfabian.303@gmail.com>
2025-07-21 17:16:42 -0600

    Create terms page

---
+++ lib/features/privacy_terms/presentation/pages/terms_page.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_conf_latam/core/widgets/container/footer.dart';
+import 'package:flutter_conf_latam/features/privacy_terms/presentation/view_model/privacy_terms_view_model.dart';
+import 'package:flutter_conf_latam/features/privacy_terms/presentation/widgets/markdown_container.dart';
+import 'package:flutter_conf_latam/l10n/localization_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class TermsPage extends ConsumerStatefulWidget {
+  const TermsPage({super.key});
+
+  @override
+  ConsumerState<TermsPage> createState() => _TermsPageState();
+}
+
+class _TermsPageState extends ConsumerState<TermsPage> {
+  final analytics = FirebaseAnalytics.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    analytics.logScreenView(screenName: 'terms_page');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverList(
+          delegate: SliverChildListDelegate([
+            const _TermsMain(),
+            const Footer(),
+          ]),
+        ),
+      ],
+    );
+  }
+}
+
+class _TermsMain extends ConsumerWidget {
+  const _TermsMain();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = ref.watch(appLocalizationsProvider);
+    final privacyPolicyData = ref.watch(
+      privacyPolicyProvider(Locale(l10n.localeName)),
+    );
+
+    return privacyPolicyData.maybeWhen(
+      data: (data) => MarkdownContainer(markdownData: data),
+      orElse: Offstage.new,
+    );
+  }
+}
