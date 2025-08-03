74e07b4bd6a529a83dce7a2c86a2ea0c594a932c
FabianVarela <jfabian.303@gmail.com>
2025-08-02 19:58:38 -0500

    Add speakers main section

--- lib/features/speakers/presentation/pages/speakers_page.dart
+++ lib/features/speakers/presentation/pages/speakers_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_conf_latam/core/widgets/container/footer.dart';
+import 'package:flutter_conf_latam/features/speakers/presentation/widgets/speakers_main.dart';
 
 class SpeakersPage extends StatefulWidget {
   const SpeakersPage({super.key});
@@ -24,7 +25,7 @@ class _SpeakersPageState extends State<SpeakersPage> {
       slivers: <Widget>[
         SliverList(
           delegate: SliverChildListDelegate([
-            // const SpeakersMain(),
+            const SpeakersMain(),
             const Footer(),
           ]),
         ),
