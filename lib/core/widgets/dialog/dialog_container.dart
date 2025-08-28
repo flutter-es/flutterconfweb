d01436d5acc159506698ce7348e0bac692e7ae58
FabianVarela <jfabian.303@gmail.com>
2025-08-28 15:58:25 -0500

    Create dialog container to reuse

---
+++ lib/core/widgets/dialog/dialog_container.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_conf_latam/core/responsive/responsive_context_layout.dart';
+import 'package:flutter_conf_latam/core/widgets/icons/circle_icon.dart';
+import 'package:flutter_conf_latam/styles/core/colors.dart';
+
+class DialogContainer extends StatelessWidget {
+  const DialogContainer({required this.child, super.key});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: FlutterLatamColors.darkBlue,
+      child: SizedBox(
+        width: double.infinity,
+        child: Stack(
+          children: <Widget>[
+            child,
+            _CloseButton(onClose: () => Navigator.of(context).pop()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CloseButton extends StatelessWidget {
+  const _CloseButton({required this.onClose});
+
+  final VoidCallback onClose;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 20,
+      top: switch (context.screenSize) {
+        ScreenSize.extraLarge || ScreenSize.large => 20,
+        ScreenSize.normal || ScreenSize.small => 0,
+      },
+      child: InkWell(
+        onTap: onClose,
+        child: const CircleIcon(
+          child: Icon(Icons.close, color: FlutterLatamColors.white),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/speakers/presentation/pages/speaker_detail_page.dart
+++ lib/features/speakers/presentation/pages/speaker_detail_page.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_conf_latam/core/responsive/responsive_context_layout.dart';
-import 'package:flutter_conf_latam/core/widgets/icons/circle_icon.dart';
+import 'package:flutter_conf_latam/core/widgets/dialog/dialog_container.dart';
 import 'package:flutter_conf_latam/features/speakers/presentation/widgets/speaker_detail_main.dart';
-import 'package:flutter_conf_latam/styles/core/colors.dart';
 
 class SpeakerDetailPage extends StatefulWidget {
   const SpeakerDetailPage({required this.id, super.key});
@@ -28,38 +26,8 @@ class _SpeakerDetailPageState extends State<SpeakerDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: FlutterLatamColors.darkBlue,
-      child: SizedBox(
-        width: double.infinity,
-        child: Stack(
-          children: <Widget>[
-            SpeakerDetailMain(id: widget.id),
-            const _CloseButton(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _CloseButton extends StatelessWidget {
-  const _CloseButton();
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      right: 20,
-      top: switch (context.screenSize) {
-        ScreenSize.extraLarge || ScreenSize.large => 20,
-        ScreenSize.normal || ScreenSize.small => 0,
-      },
-      child: InkWell(
-        onTap: () => Navigator.of(context).pop(),
-        child: const CircleIcon(
-          child: Icon(Icons.close, color: FlutterLatamColors.white),
-        ),
-      ),
+    return DialogContainer(
+      child: SpeakerDetailMain(id: widget.id),
     );
   }
 }
