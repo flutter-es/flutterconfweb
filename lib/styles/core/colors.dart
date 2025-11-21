407bcfe576a3c04da84fa8055dd034c799133144
FabianVarela <jfabian.303@gmail.com>
2025-11-21 16:37:00 -0500

    Add colors

--- lib/styles/core/colors.dart
+++ lib/styles/core/colors.dart
@@ -5,6 +5,7 @@ class FlutterLatamColors {
   static const Color black = Color(0xFF000000);
   static const Color darkBlue = Color(0xFF0D4377);
   static const Color mainBlue = Color(0xFF042B59);
+  static const Color steelBlue = Color(0xFF095CBF);
   static const Color blue = Color(0xFF016EE8);
   static const Color mediumBlue = Color(0xFF78B7FD);
   static const Color lightBlue = Color(0xFFC9E3FF);
@@ -15,6 +16,7 @@ class FlutterLatamColors {
   static const Color red = Color(0xFFD30D19);
   static const Color mediumRed = Color(0xFFCA4D4F);
   static const Color lightRed = Color(0xFFFB8A91);
+  static const Color fuchsia = Color(0xFFE71569);
   static const Color purple = Color(0xFFB86EEE);
   static const Color pink = Color(0xFFF779E2);
   static const Color silver = Color(0xFFBABABA);
