8b26be34a6548848e0a7b3c955a44e5ca7643fa9
FabianVarela <jfabian.303@gmail.com>
2025-12-04 13:57:20 -0500

    Modify image url list

--- lib/features/home/presentation/view_model/home_view_model.dart
+++ lib/features/home/presentation/view_model/home_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter_conf_latam/core/dependencies.dart';
 import 'package:flutter_conf_latam/features/home/data/home_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -14,15 +15,17 @@ final faqListProvider = FutureProvider.family((ref, Locale locale) {
 });
 
 final galleryPreviewProvider = FutureProvider.autoDispose((ref) async {
+  final storageUrl = ref.watch(configProvider).firebaseStorageUrl;
+
   await Future<void>.delayed(const Duration(seconds: 3));
   return <String>[
-    'https://picsum.photos/seed/flutter1/800/600',
-    'https://picsum.photos/seed/flutter2/800/600',
-    'https://picsum.photos/seed/flutter3/800/600',
-    'https://picsum.photos/seed/flutter4/800/600',
-    'https://picsum.photos/seed/flutter5/800/600',
-    'https://picsum.photos/seed/flutter6/800/600',
-    'https://picsum.photos/seed/flutter7/800/600',
-    'https://picsum.photos/seed/flutter8/800/600',
+    '$storageUrl/gallery%2F06_IMG_FCL.jpg?alt=media&token=484b5873-fc53-454e-a5a9-79d9424ea6c8',
+    '$storageUrl/gallery%2F14_IMG_FCL.jpg?alt=media&token=17dc7496-3f06-47ed-a417-d3f73682485f',
+    '$storageUrl/gallery%2F16_IMG_FCL.jpg?alt=media&token=d23bbfbd-5fc8-4a1d-a156-95e7b01a03d9',
+    '$storageUrl/gallery%2F10_IMG_FCL.jpg?alt=media&token=429c6612-15a2-4db5-a545-e7c822a3cb1b',
+    '$storageUrl/gallery%2F26_IMG_FCL.jpg?alt=media&token=d51ac358-aacd-4ec1-a524-c5f6cd74d05a',
+    '$storageUrl/gallery%2F33_IMG_FCL.jpg?alt=media&token=6c024ed1-984d-4873-ae21-4da6dccf8ee1',
+    '$storageUrl/gallery%2F34_IMG_FCL.jpg?alt=media&token=0e79a9c1-13d4-4402-925d-e6b13229a738',
+    '$storageUrl/gallery%2F15_IMG_FCL.jpg?alt=media&token=a14a0693-7d81-4c09-bc90-3377c2648a51',
   ];
 });
--- lib/features/home/presentation/widgets/home_gallery.dart
+++ lib/features/home/presentation/widgets/home_gallery.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_conf_latam/core/responsive/responsive_context_layout.dart';
 import 'package:flutter_conf_latam/core/widgets/container/carousel_container.dart';
+import 'package:flutter_conf_latam/core/widgets/container/keep_alive_container.dart';
 import 'package:flutter_conf_latam/core/widgets/container/section_container.dart';
 import 'package:flutter_conf_latam/core/widgets/container/shimmer_container.dart';
 import 'package:flutter_conf_latam/core/widgets/images/single_image.dart';
@@ -19,38 +20,40 @@ class HomeGallery extends ConsumerWidget {
       _ => const Size.square(264),
     };
 
-    return SectionContainer(
-      spacing: 0,
-      children: <Widget>[
-        gallery.maybeWhen(
-          data: (data) {
-            if (data.isEmpty) return const Offstage();
-            return CarouselContainer(
-              itemSize: size,
-              items: <Widget>[
-                for (final item in data)
-                  Center(
-                    child: SingleImage(imageUrl: item, size: size),
-                  ),
-              ],
-            );
-          },
-          loading: () => Shimmer(
-            child: CarouselContainer(
-              itemSize: size,
-              items: .generate(20, (_) {
-                return Center(
-                  child: ShimmerLoading(
-                    isLoading: true,
-                    child: SingleImageContainer(size: size),
-                  ),
-                );
-              }),
+    return KeepAliveContainer(
+      child: SectionContainer(
+        spacing: 0,
+        children: <Widget>[
+          gallery.maybeWhen(
+            data: (data) {
+              if (data.isEmpty) return const Offstage();
+              return CarouselContainer(
+                itemSize: size,
+                items: <Widget>[
+                  for (final item in data)
+                    Center(
+                      child: SingleImage(imageUrl: item, size: size),
+                    ),
+                ],
+              );
+            },
+            loading: () => Shimmer(
+              child: CarouselContainer(
+                itemSize: size,
+                items: .generate(20, (_) {
+                  return Center(
+                    child: ShimmerLoading(
+                      isLoading: true,
+                      child: SingleImageContainer(size: size),
+                    ),
+                  );
+                }),
+              ),
             ),
+            orElse: Offstage.new,
           ),
-          orElse: Offstage.new,
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
