202e6bbf63b30ed997f73bd4cbb9948a7a789911
FabianVarela <jfabian.303@gmail.com>
2026-06-22 13:35:36 -0500

    Set timeout if fails

--- lib/core/utils/page_preloader.dart
+++ lib/core/utils/page_preloader.dart
@@ -1,13 +1,16 @@
 import 'package:signals/signals.dart';
 
 class PagePreloader {
-  static Future<void> preload(List<FutureSignal<dynamic>> signals) async {
+  static Future<void> preload(
+    List<FutureSignal<dynamic>> signals, {
+    Duration timeout = const Duration(seconds: 10),
+  }) async {
     await Future.wait(
-      signals.map(
-        (signal) => signal.future.catchError((_) {
-          return null;
-        }),
-      ),
+      signals.map((signal) {
+        return signal.future
+            .catchError((_) => null)
+            .timeout(timeout, onTimeout: () => null);
+      }),
     );
   }
 }
