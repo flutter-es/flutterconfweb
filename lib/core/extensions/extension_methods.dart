cf4ab8752260a0e46ee4ca3f543611866b4b80e2
FabianVarela <jfabian.303@gmail.com>
2026-07-01 17:37:58 -0500

    Create extension methods

--- lib/core/extensions/extension_methods.dart
+++ lib/core/extensions/extension_methods.dart
@@ -1,3 +1,23 @@
+import 'dart:ui';
+
+import 'package:flutter_conf_core/flutter_conf_core.dart';
+
 extension StringX on String {
   bool get isValidUrl => Uri.parse(this).isAbsolute;
+
+  Color get toColor {
+    final h = replaceAll('#', '');
+    return Color(int.parse('FF$h', radix: 16));
+  }
+}
+
+extension DateTimeX on DateTime {
+  TicketPricingEntity? ticketDate(List<TicketPricingEntity> data) {
+    for (final ticket in data) {
+      if (!isBefore(ticket.startDate) && !isAfter(ticket.endDate)) {
+        return ticket;
+      }
+    }
+    return null;
+  }
 }
